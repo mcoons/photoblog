ffa04c2cbb37157a33d6f55eb44ff9304ce15112
Michael Coons <michael@Michaels-MacBook-Pro-5.local>
2018-08-31 14:56:36 -0600

    fixed package.json

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 const bodyParser = require("body-parser");
-require('dotenv').config();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const expressSanitizer = require("express-sanitizer");
 
-mongoose.connect(process.env.MONGODB);
+mongoose.connect(process.env.MONGODB2, { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
