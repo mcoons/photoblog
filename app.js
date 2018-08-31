01f6465aaa6287204022365671e4cfa6f866b711
Michael Coons <michael@Michaels-MacBook-Pro-5.local>
2018-08-31 14:35:44 -0600

    preparing for Heroku publishing

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 const bodyParser = require("body-parser");
+require('dotenv').config();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const expressSanitizer = require("express-sanitizer");
 
-mongoose.connect("mongodb://localhost/restful_blog_app");
+mongoose.connect(process.env.MONGODB);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
